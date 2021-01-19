023dd74ff01d7fb9ea15efa25b67159aeba513aa
Mark Heinemann <mheinemann@r3aig.com>
2021-01-19 14:20:55 +0000

    merged

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ print("hello world")
 
 print(" more stuff")
 
-<<<<<<< HEAD
+
 print("  added to branch mycode")
 
 
@@ -12,10 +12,10 @@ print( " 1st comment from win7 on line7")master
 
 
 print (" 1st comment from ubuntu")
-=======
+
 print( "comment added whilst on mycode branch") 
 print( "another comment added whilst on mycode branch")
->>>>>>> mycode
+
 
 print(" the  really latest comment")mycode
 
