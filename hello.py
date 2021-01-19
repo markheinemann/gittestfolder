b8f8d935d6375646953dbd165217d9b03376f554
Mark Heinemann <mheinemann@r3aig.com>
2021-01-19 14:06:17 +0000

    add master comment

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ print(" more stuff")
 print("  added to branch mycode")
 
 
-print( " 1st comment from win7 on line7")
+print( " 1st comment from win7 on line7")master
 
 
 print (" 1st comment from ubuntu")
