87d78e1a1fc6cbf543d3952cc8710d64022529e0
Mark Heinemann <mheinemann@r3aig.com>
2021-01-19 14:08:43 +0000

    comment mycode

--- hello.py
+++ hello.py
@@ -4,10 +4,10 @@ print("hello world")
 
 print("more stuff")
 
-print( "comment added whilst on mycode branch")
+print( "comment added whilst on mycode branch") 
 print( "another comment added whilst on mycode branch")
 
-print(" the  really latest comment")
+print(" the  really latest comment")mycode
 
 
 # end of the file
